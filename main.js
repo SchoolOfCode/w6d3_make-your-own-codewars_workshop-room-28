a6514031b45291098c57968d1d9ed6aea927d5ca
unknown <kavita.kaur998@gmail.com>
2021-12-22 18:22:18 +0000

    tasks 1 & 2 complete

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-/* 
-👉 Write your kata here!
-*/
+// Christmas Pudding 
+// 👉 Write your kata here!
+// You have been asked to make christmas pudding for the big day. Using the recipe below (recipe), calculate how many puddings you can make given the ingredients you have in your cupboard (cupboard). 
+
+// The ingredients in your cupboard are given as an array of objects where each ingredient is paired with its quantity (string). See example 1 below. 
+
+// You must return a Number. 
+
+// recipe:
+//     2 cups dried fruit
+//     1 cup flour
+//     1 cup butter 
+//     1 cup breadcrumbs
+//     1 cup sugar
+//     1 cup nuts
+//     1 cup milk
+//     1 egg
+
+
+// Example 1 whats in the cupboard: 
+//  cupboard =  
+//    [
+//         {driedFruit : '6 cups'},
+//         {Flour: '2 cups' },
+//         {Butter: '2 cups'},
+//         {breadcrumbs:'2 cups'},
+//         {sugar:'2 cups'},
+//         {nuts: '2 cups'},
+//         {milk: '2 cups'},
+//         {eggs:'2'}
+//     ]
+
+// Ans: dried mixed fruit can yield 3 puddings but the rest only yield 2 so the output is 2. 
+
+
+
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function calculateChristmasPuddings(cupboard){
+  
+};
+
